523b625d5e69ed3ed9ae28ee91b62af5531cde50
Evan Phoenix <evan@phx.io>
2013-12-10 14:58:12 -0800

    BIG BIRTHDAY!

---
+++ patch.go
@@ -0,0 +1,481 @@
+package jsonpatch
+
+import (
+  "bytes"
+  "encoding/json"
+  "fmt"
+  "strconv"
+  "strings"
+)
+
+const (
+  eRaw = 0
+  eDoc = 1
+  eAry = 2
+)
+
+type LazyNode struct {
+  raw *json.RawMessage
+  doc PartialDoc
+  ary PartialArray
+  which int
+}
+
+func newLazyNode(raw *json.RawMessage) *LazyNode {
+  return &LazyNode { raw: raw, doc: nil, ary: nil, which: eRaw }
+}
+
+func (n *LazyNode) MarshalJSON() ([]byte, error) {
+  switch n.which {
+  case eRaw:
+    return *n.raw, nil
+  case eDoc:
+    return json.Marshal(n.doc)
+  case eAry:
+    return json.Marshal(n.ary)
+  default:
+    return nil, fmt.Errorf("Unknown type")
+  }
+}
+
+func (n *LazyNode) UnmarshalJSON(data []byte) error {
+  dest := make(json.RawMessage, len(data))
+  copy(dest, data)
+  n.raw = &dest
+  n.which = eRaw
+  return nil
+}
+
+func (n *LazyNode) IntoDoc() (*PartialDoc, error) {
+  if n.which == eDoc {
+    return &n.doc, nil
+  }
+
+  err := json.Unmarshal(*n.raw, &n.doc)
+
+  if err != nil {
+    return nil, err
+  }
+
+  n.which = eDoc
+  return &n.doc, nil
+}
+
+func (n *LazyNode) IntoAry() (*PartialArray, error) {
+  if n.which == eAry {
+    return &n.ary, nil
+  }
+
+  err := json.Unmarshal(*n.raw, &n.ary)
+
+  if err != nil {
+    return nil, err
+  }
+
+  n.which = eAry
+  return &n.ary, nil
+}
+
+func (n *LazyNode) compact() []byte {
+  buf := new(bytes.Buffer)
+
+  err := json.Compact(buf, *n.raw)
+
+  if err != nil {
+    return *n.raw
+  }
+
+  return buf.Bytes()
+}
+
+func (n *LazyNode) tryDoc() bool {
+  err := json.Unmarshal(*n.raw, &n.doc)
+
+  if err != nil {
+    return false
+  }
+
+  n.which = eDoc
+  return true
+}
+
+func (n *LazyNode) tryAry() bool {
+  err := json.Unmarshal(*n.raw, &n.ary)
+
+  if err != nil {
+    return false
+  }
+
+  n.which = eAry
+  return true
+}
+
+func (n *LazyNode) equal(o *LazyNode) bool {
+  if n.which == eRaw {
+    if !n.tryDoc() && !n.tryAry() {
+      if o.which != eRaw {
+        return false
+      }
+
+      return bytes.Equal(n.compact(), o.compact())
+    }
+  }
+
+  if n.which == eDoc {
+    if o.which == eRaw {
+      if !o.tryDoc() {
+        return false
+      }
+    }
+
+    if o.which != eDoc {
+      return false
+    }
+
+    for k,v := range n.doc {
+      ov, ok := o.doc[k]
+
+      if !ok {
+        return false
+      }
+
+      if !v.equal(ov) {
+        return false
+      }
+    }
+
+    return true
+  }
+
+  if len(n.ary) != len(o.ary) {
+    return false
+  }
+
+  for idx, val := range n.ary {
+    if !val.equal(o.ary[idx]) {
+      return false
+    }
+  }
+
+  return true
+}
+
+type Operation map[string]*json.RawMessage
+type Patch []Operation
+
+type PartialDoc map[string]*LazyNode
+type PartialArray []*LazyNode
+
+type Container interface {
+  Get(key string) (*LazyNode, error)
+  Set(key string, val *LazyNode) error
+  Remove(key string) error
+}
+
+func DecodePatch(buf []byte) (Patch, error) {
+  var p Patch
+
+  err := json.Unmarshal(buf, &p)
+
+  if err != nil {
+    return nil, err
+  }
+
+  return p, nil
+}
+
+func (p Patch) Operation(i int) Operation {
+  if i < 0 || i >= len(p) {
+    return nil
+  }
+
+  return p[i]
+}
+
+func (o Operation) Kind() string {
+  if obj, ok := o["op"]; ok {
+    var op string
+
+    err := json.Unmarshal(*obj, &op)
+
+    if err != nil {
+      return "unknown"
+    }
+
+    return op
+  }
+
+  return "unknown"
+}
+
+func (o Operation) Path() string {
+  if obj, ok := o["path"]; ok {
+    var op string
+
+    err := json.Unmarshal(*obj, &op)
+
+    if err != nil {
+      return "unknown"
+    }
+
+    return op
+  }
+
+  return "unknown"
+}
+
+func (o Operation) From() string {
+  if obj, ok := o["from"]; ok {
+    var op string
+
+    err := json.Unmarshal(*obj, &op)
+
+    if err != nil {
+      return "unknown"
+    }
+
+    return op
+  }
+
+  return "unknown"
+}
+
+func (o Operation) Value() *LazyNode {
+  if obj, ok := o["value"]; ok {
+    return newLazyNode(obj)
+  }
+
+  return nil
+}
+
+func isArray(buf []byte) bool {
+  for _, c := range buf {
+    switch(c) {
+    case ' ':
+    case '\n':
+    case '\t':
+      continue
+    case '[':
+      return true
+    default:
+      break
+    }
+  }
+
+  return false
+}
+
+func findObject(doc *PartialDoc, path string) (Container, string) {
+  split := strings.Split(path, "/")
+
+  parts := split[1:len(split)-1]
+
+  key := split[len(split)-1]
+
+  var err error
+
+  for idx, part := range parts {
+    next, ok := (*doc)[part]
+    if !ok {
+      return nil, ""
+    }
+
+    if isArray(*next.raw) {
+      if idx == len(parts) - 1 {
+        ary, err := next.IntoAry()
+
+        if err != nil {
+          return nil, ""
+        }
+
+        return ary, key
+      } else {
+        return nil, ""
+      }
+    } else {
+      doc, err = next.IntoDoc()
+
+      if err != nil {
+        return nil, ""
+      }
+    }
+  }
+
+  return doc, key
+}
+
+func (d *PartialDoc) Set(key string, val *LazyNode) error {
+  (*d)[key] = val
+  return nil
+}
+
+func (d *PartialDoc) Get(key string) (*LazyNode, error) {
+  return (*d)[key], nil
+}
+
+func (d *PartialDoc) Remove(key string) error {
+  delete(*d, key)
+  return nil
+}
+
+func (d *PartialArray) Set(key string, val *LazyNode) error {
+  if key == "-" {
+    *d = append(*d, val)
+    return nil
+  }
+
+  idx, err := strconv.Atoi(key)
+
+  if err != nil {
+    return err
+  }
+
+  ary := make([]*LazyNode, len(*d) + 1)
+
+  cur := *d
+
+  copy(ary[0:idx], cur[0:idx])
+  ary[idx] = val
+  copy(ary[idx+1:], cur[idx:])
+
+  *d = ary
+  return nil
+}
+
+func (d *PartialArray) Get(key string) (*LazyNode, error) {
+  idx, err := strconv.Atoi(key)
+
+  if err != nil {
+    return nil, err
+  }
+
+  return (*d)[idx], nil
+}
+
+func (d *PartialArray) Remove(key string) error {
+  idx, err := strconv.Atoi(key)
+
+  if err != nil {
+    return err
+  }
+
+  cur := *d
+
+  ary := make([]*LazyNode, len(cur) - 1)
+
+  copy(ary[0:idx], cur[0:idx])
+  copy(ary[idx:], cur[idx+1:])
+
+  *d = ary
+  return nil
+
+}
+
+func (p Patch) add(doc *PartialDoc, op Operation) error {
+  path := op.Path()
+
+  con, key := findObject(doc, path)
+
+  if con == nil {
+    return fmt.Errorf("Missing container: %s", path)
+  }
+
+  con.Set(key, op.Value())
+
+  return nil
+}
+
+func (p Patch) remove(doc *PartialDoc, op Operation) error {
+  path := op.Path()
+
+  con, key := findObject(doc, path)
+
+  return con.Remove(key)
+}
+
+func (p Patch) replace(doc *PartialDoc, op Operation) error {
+  path := op.Path()
+
+  con, key := findObject(doc, path)
+
+  con.Set(key, op.Value())
+
+  return nil
+}
+
+func (p Patch) move(doc *PartialDoc, op Operation) error {
+  from := op.From()
+
+  con, key := findObject(doc, from)
+
+  val, err := con.Get(key)
+
+  if err != nil {
+    return err
+  }
+
+  con.Remove(key)
+
+  path := op.Path()
+
+  con, key = findObject(doc, path)
+
+  con.Set(key, val)
+
+  return nil
+}
+
+var eTestFailed = fmt.Errorf("Testing value failed")
+
+func (p Patch) test(doc *PartialDoc, op Operation) error {
+  path := op.Path()
+
+  con, key := findObject(doc, path)
+
+  val, err := con.Get(key)
+
+  if err != nil {
+    return err
+  }
+
+  if val.equal(op.Value()) {
+    return nil
+  }
+
+  return eTestFailed
+}
+
+func (p Patch) Apply(doc []byte) ([]byte, error) {
+  pd := new(PartialDoc)
+
+  err := json.Unmarshal(doc, pd)
+
+  if err != nil {
+    return nil, err
+  }
+
+  err = nil
+
+  for _, op := range p {
+    switch op.Kind() {
+    case "add":
+      err = p.add(pd, op)
+    case "remove":
+      err = p.remove(pd, op)
+    case "replace":
+      err = p.replace(pd, op)
+    case "move":
+      err = p.move(pd, op)
+    case "test":
+      err = p.test(pd, op)
+    default:
+      err = fmt.Errorf("Unexpected kind: %s", op.Kind())
+    }
+
+    if err != nil {
+      return nil, err
+    }
+  }
+
+  return json.Marshal(pd)
+}
---
+++ patch_test.go
@@ -0,0 +1,190 @@
+package jsonpatch
+
+import (
+  "encoding/json"
+  "testing"
+)
+
+func reformatJSON(j string) string {
+  var doc PartialDoc
+
+  err := json.Unmarshal([]byte(j), &doc)
+
+  if err != nil {
+    panic(err)
+  }
+
+  b, err := json.MarshalIndent(doc, "", "  ")
+
+  if err != nil {
+    panic(err)
+  }
+
+  return string(b)
+}
+
+func compareJSON(a, b string) bool {
+  return reformatJSON(a) == reformatJSON(b)
+}
+
+func applyPatch(doc, patch string) (string, error) {
+  obj, err := DecodePatch([]byte(patch))
+
+  if err != nil {
+    panic(err)
+  }
+
+  out, err := obj.Apply([]byte(doc))
+
+  if err != nil {
+    return "", err
+  }
+
+  return string(out), nil
+}
+
+type Case struct {
+  doc, patch, result string
+}
+
+var Cases = []Case {
+  {
+    `{ "foo": "bar"}`,
+    `[
+         { "op": "add", "path": "/baz", "value": "qux" }
+     ]`,
+     `{
+       "baz": "qux",
+       "foo": "bar"
+     }`,
+  },
+  {
+   `{ "foo": [ "bar", "baz" ] }`,
+   `[
+     { "op": "add", "path": "/foo/1", "value": "qux" }
+    ]`,
+    `{ "foo": [ "bar", "qux", "baz" ] }`,
+  },
+  {
+    `{ "baz": "qux", "foo": "bar" }`,
+    `[ { "op": "remove", "path": "/baz" } ]`,
+    `{ "foo": "bar" }`,
+  },
+  {
+    `{ "foo": [ "bar", "qux", "baz" ] }`,
+    `[ { "op": "remove", "path": "/foo/1" } ]`,
+    `{ "foo": [ "bar", "baz" ] }`,
+  },
+  {
+    `{ "baz": "qux", "foo": "bar" }`,
+    `[ { "op": "replace", "path": "/baz", "value": "boo" } ]`,
+    `{ "baz": "boo", "foo": "bar" }`,
+  },
+  {
+   `{
+     "foo": {
+       "bar": "baz",
+       "waldo": "fred"
+     },
+     "qux": {
+       "corge": "grault"
+     }
+   }`,
+   `[ { "op": "move", "from": "/foo/waldo", "path": "/qux/thud" } ]`,
+   `{
+     "foo": {
+       "bar": "baz"
+     },
+     "qux": {
+       "corge": "grault",
+       "thud": "fred"
+     }
+   }`,
+  },
+  {
+    `{ "foo": [ "all", "grass", "cows", "eat" ] }`,
+    `[ { "op": "move", "from": "/foo/1", "path": "/foo/3" } ]`,
+    `{ "foo": [ "all", "cows", "eat", "grass" ] }`,
+  },
+  {
+    `{ "foo": "bar" }`,
+    `[ { "op": "add", "path": "/child", "value": { "grandchild": { } } } ]`,
+    `{ "foo": "bar", "child": { "grandchild": { } } }`,
+  },
+  {
+    `{ "foo": ["bar"] }`,
+    `[ { "op": "add", "path": "/foo/-", "value": ["abc", "def"] } ]`,
+    `{ "foo": ["bar", ["abc", "def"]] }`,
+  },
+}
+
+type BadCase struct {
+  doc, patch string
+}
+
+var BadCases = []BadCase {
+  {
+    `{ "foo": "bar" }`,
+    `[ { "op": "add", "path": "/baz/bat", "value": "qux" } ]`,
+  },
+}
+
+func TestAllCases(t *testing.T) {
+  for _, c := range Cases {
+   out, err := applyPatch(c.doc, c.patch)
+
+   if err != nil {
+     t.Errorf("Unable to apply patch: %s", err)
+   }
+
+   if !compareJSON(out, c.result) {
+     t.Errorf("Patch did not apply. Expected:\n%s\n\nActual:\n%s",
+      reformatJSON(c.result), reformatJSON(out))
+   }
+  }
+
+  for _, c := range BadCases {
+    _, err := applyPatch(c.doc, c.patch)
+
+    if err == nil {
+      t.Errorf("Patch should have failed to apply but it did not")
+    }
+  }
+}
+
+type TestCase struct {
+  doc, patch string
+  result bool
+}
+
+var TestCases = []TestCase {
+  {
+    `{
+       "baz": "qux",
+       "foo": [ "a", 2, "c" ]
+     }`,
+    `[
+       { "op": "test", "path": "/baz", "value": "qux" },
+       { "op": "test", "path": "/foo/1", "value": 2 }
+     ]`,
+     true,
+  },
+  {
+    `{ "baz": "qux" }`,
+    `[ { "op": "test", "path": "/baz", "value": "bar" } ]`,
+    false,
+  },
+}
+
+
+func TestAllTest(t *testing.T) {
+  for _, c := range TestCases {
+    _, err := applyPatch(c.doc, c.patch)
+
+    if c.result && err != nil {
+      t.Errorf("Testing failed when it should have passed: %s", err)
+    } else if !c.result && err == nil {
+      t.Errorf("Testing passed when it should have faild", err)
+    }
+  }
+}
