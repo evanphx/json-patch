edfb4cb2f55c50296905198656dc50891c2cd81b
Jimmi Dyson <jimmidyson@gmail.com>
2016-06-06 14:04:27 +0100

    Escape / and ~ as per http://tools.ietf.org/html/rfc6901#section-4

--- merge_test.go
+++ merge_test.go
@@ -361,3 +361,33 @@ func TestMergeObjectWithInnerArray(t *testing.T) {
 		t.Fatalf("Patch should have been {} but was: %v", string(patch))
 	}
 }
+
+func TestMergeReplaceKeyRequiringEscape(t *testing.T) {
+	doc := `{ "title": "hello", "nested": {"title/escaped": 1, "two": 2} }`
+	pat := `{ "title": "goodbye", "nested": {"title/escaped": 2, "two": 2}  }`
+
+	exp := `{ "title": "goodbye", "nested": {"title~1escaped": 2}  }`
+
+	res, err := CreateMergePatch([]byte(doc), []byte(pat))
+
+	if err != nil {
+		t.Errorf("Unexpected error: %s, %s", err, string(res))
+	}
+
+	if !compareJSON(exp, string(res)) {
+		t.Log(string(res))
+		t.Fatalf("Key was not replaced")
+	}
+}
+
+func TestMergePatchReplaceKeyRequiringEscaping(t *testing.T) {
+	doc := `{ "obj": { "title/escaped": "hello" } }`
+	pat := `{ "obj": { "title~1escaped": "goodbye" } }`
+	exp := `{ "obj": { "title/escaped": "goodbye" } }`
+
+	res := mergePatch(doc, pat)
+
+	if !compareJSON(exp, res) {
+		t.Fatalf("Key was not replaced")
+	}
+}
