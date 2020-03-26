9f7d3fd5c24bae2057d228018cf2254d79336d7d
Benjamin Elder <ben@elder.dev>
2020-03-26 14:38:20 -0700

    update paths for v5

--- v5/cmd/json-patch/main.go
+++ v5/cmd/json-patch/main.go
@@ -6,7 +6,7 @@ import (
 	"log"
 	"os"
 
-	jsonpatch "github.com/evanphx/json-patch"
+	jsonpatch "github.com/evanphx/json-patch/v5"
 	flags "github.com/jessevdk/go-flags"
 )
 
--- v5/go.mod
+++ v5/go.mod
@@ -1,4 +1,4 @@
-module github.com/evanphx/json-patch
+module github.com/evanphx/json-patch/v5
 
 go 1.12
 
