42bcdf26fca564b75f23a698b49635b5b2714c8d
Sebastiaan van Stijn <github@gone.nl>
2020-08-05 17:42:21 +0200

    Revert "Test for copying non-existent key."
    This reverts commit caa7f266ac971d84049d2a618644979d26114ac8.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- patch_test.go
+++ patch_test.go
@@ -336,11 +336,6 @@ var BadCases = []BadCase{
 		`{ "baz": "qux" }`,
 		`[ { "op": "replace", "path": "/foo", "value": "bar" } ]`,
 	},
-	// Can't copy from non-existent "from" key.
-	{
-		`{ "foo": "bar"}`,
-		`[{"op": "copy", "path": "/qux", "from": "/baz"}]`,
-	},
 }
 
 // This is not thread safe, so we cannot run patch tests in parallel.
