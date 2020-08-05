4c18d304a429fb056cb4861df99fbcaa9107164b
Sebastiaan van Stijn <github@gone.nl>
2020-08-05 17:42:26 +0200

    Revert "Conform to RFC6902 replacement semantics."
    This reverts commit a5df74aab0efb032ca893c048330abcad0b78e6f.
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- patch.go
+++ patch.go
@@ -392,17 +392,13 @@ func (d *partialDoc) add(key string, val *lazyNode) error {
 }
 
 func (d *partialDoc) get(key string) (*lazyNode, error) {
-	v, ok := (*d)[key]
-	if !ok {
-		return v, errors.Wrapf(ErrMissing, "unable to get nonexistent key: %s", key)
-	}
-	return v, nil
+	return (*d)[key], nil
 }
 
 func (d *partialDoc) remove(key string) error {
 	_, ok := (*d)[key]
 	if !ok {
-		return errors.Wrapf(ErrMissing, "unable to remove nonexistent key: %s", key)
+		return errors.Wrapf(ErrMissing, "Unable to remove nonexistent key: %s", key)
 	}
 
 	delete(*d, key)
@@ -624,7 +620,7 @@ func (p Patch) test(doc *container, op Operation) error {
 	}
 
 	val, err := con.get(key)
-	if err != nil && errors.Cause(err) != ErrMissing {
+	if err != nil {
 		return errors.Wrapf(err, "error in test for path: '%s'", path)
 	}
 
