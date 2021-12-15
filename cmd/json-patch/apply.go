24c5a2b51054c084b976bed477ec3f50993fcf18
Jeffrey Falgout <jeffrey.falgout@gmail.com>
2021-12-14 19:17:39 -0700

    Maintain reverse compatibility with json-patch.
    The root json-patch command should now behave the same as it used to.

--- cmd/json-patch/apply.go
+++ cmd/json-patch/apply.go
@@ -12,14 +12,67 @@ import (
 	cobra "github.com/spf13/cobra"
 )
 
-var (
-	patchFiles []string
-	patch      patcher = rfc6902{}
-	indent     string
-)
-
 type patcher interface {
 	patch(json []byte, patches [][]byte) ([]byte, error)
+}
+
+func init() {
+	var patchFiles []string
+	var patch patcher = rfc7396{}
+	var indent string
+
+	cmd := &cobra.Command{
+		Use:   "apply [flags] [JSON document]",
+		Short: "Apply one or more JSON patches to a JSON document.",
+		Long: `Apply one or more JSON patches to a JSON document.
+
+The JSON document file can either be passed in as an argument, or piped through stdin.`,
+		Args: cobra.MaximumNArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return apply(cmd, args, patchFiles, patch, indent)
+		},
+	}
+	cmd.Flags().VarP(FilesFlag{&patchFiles}, "patch-file", "p", "Path to file with one or more operations")
+	cmd.Flags().VarP(patcherFlag{&patch}, "patch-format", "f", "The format of the patches. One of RFC6902 or RFC7396.")
+	cmd.Flags().StringVar(&indent, "indent", "", "What indent to use when formatting the result.")
+
+	rootCmd.AddCommand(cmd)
+}
+
+func apply(cmd *cobra.Command, args []string, patchFiles []string, patch patcher, indent string) error {
+	var patches [][]byte
+	for _, p := range patchFiles {
+		b, err := ioutil.ReadFile(p)
+		if err != nil {
+			return fmt.Errorf("could not read %q: %w", p, err)
+		}
+		patches = append(patches, b)
+	}
+
+	var doc []byte
+	var err error
+	if len(args) == 0 {
+		doc, err = ioutil.ReadAll(os.Stdin)
+	} else {
+		doc, err = ioutil.ReadFile(args[0])
+	}
+	if err != nil {
+		return err
+	}
+
+	res, err := patch.patch(doc, patches)
+	if err != nil {
+		return err
+	}
+	if indent != "" {
+		var b bytes.Buffer
+		if err := json.Indent(&b, res, "", indent); err != nil {
+			return err
+		}
+		res = b.Bytes()
+	}
+	fmt.Println(string(res))
+	return nil
 }
 
 type rfc6902 struct{}
@@ -55,57 +108,6 @@ func (rfc7396) patch(json []byte, patches [][]byte) ([]byte, error) {
 		}
 	}
 	return json, nil
-}
-
-func init() {
-	cmd := &cobra.Command{
-		Use:   "apply [flags] [JSON document]",
-		Short: "Apply one or more JSON patches to a JSON document.",
-		Long: `Apply one or more JSON patches to a JSON document.
-
-The JSON document file can either be passed in as an argument, or piped through stdin.`,
-		Args: cobra.MaximumNArgs(1),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			var patches [][]byte
-			for _, p := range patchFiles {
-				b, err := ioutil.ReadFile(p)
-				if err != nil {
-					return fmt.Errorf("could not read %q: %w", p, err)
-				}
-				patches = append(patches, b)
-			}
-
-			var doc []byte
-			var err error
-			if len(args) == 0 {
-				doc, err = ioutil.ReadAll(os.Stdin)
-			} else {
-				doc, err = ioutil.ReadFile(args[0])
-			}
-			if err != nil {
-				return err
-			}
-
-			res, err := patch.patch(doc, patches)
-			if err != nil {
-				return err
-			}
-			if indent != "" {
-				var b bytes.Buffer
-				if err := json.Indent(&b, res, "", indent); err != nil {
-					return err
-				}
-				res = b.Bytes()
-			}
-			fmt.Println(string(res))
-			return nil
-		},
-	}
-	cmd.Flags().VarP(FilesFlag{&patchFiles}, "patch-file", "p", "Path to file with one or more operations")
-	cmd.Flags().VarP(patcherFlag{&patch}, "patch-format", "f", "The format of the patches. One of RFC6902 or RFC7396.")
-	cmd.Flags().StringVar(&indent, "indent", "", "What indent to use when formatting the result.")
-
-	rootCmd.AddCommand(cmd)
 }
 
 type patcherFlag struct {
--- cmd/json-patch/main.go
+++ cmd/json-patch/main.go
@@ -10,11 +10,28 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "json-patch",
 	Short: "A tool for RFC6902 and RFC7396 JSON manipulation.",
+	Long: `A tool for RFC6902 and RFC7396 JSON manipulation.
+
+For reverse compatiblity with a previous version of this tool, invoking this tool without any subcommand is the same as` + " `apply -f RFC6902`.",
+	CompletionOptions: cobra.CompletionOptions{
+		// TODO: https://github.com/spf13/cobra/issues/1507
+		// Set HiddenDefaultCmd = true and clear DisableDefaultCmd.
+		DisableDefaultCmd: true,
+	},
+}
+
+func init() {
+	var patchFiles []string
+
+	// Add RunE to the root command for reverse compatibility. We should probably deprecate this and encourage users to use apply directly.
+	rootCmd.RunE =
+		func(cmd *cobra.Command, args []string) error {
+			return apply(cmd, args, patchFiles, rfc6902{}, "")
+		}
+	rootCmd.Flags().VarP(FilesFlag{&patchFiles}, "patch-file", "p", "Path to file with one or more operations")
 }
 
 func main() {
-	// TODO: Hide the completion command once we're able. This capability was only added recently: https://github.com/spf13/cobra/issues/1507
-	// rootCmd.CompletionOptions.HiddenDefaultCmd = true
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 		os.Exit(1)
